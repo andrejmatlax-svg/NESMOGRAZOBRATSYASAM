a61b2a6119ed7696a1aa6ccaae18f7436174098c
Andrey <Matlah>
2026-06-03 20:25:41 +0300

    array333

--- lesson6.playground/Contents.swift
+++ lesson6.playground/Contents.swift
@@ -934,6 +934,28 @@ do{
 
 
 
+let array333: [Int] = [ 1,2,3,4,5]
+
+
+
+func getValue7(index: Int) throws -> Int {
+    guard array333.count > index && index >= 0   else {
+      throw MyError2.myError2
+    }
+    return array333[index]
+}
+
+do{
+    let value = try getValue7(index: 55)
+    print(value)
+} catch {
+    print(error)
+}
+
+
+
+
+
 
 
 
